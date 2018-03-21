LightBitsLabs/procstat

commit e59946bf049181d857a62e96dd36cbd38aeb573a

procstat: fix refcount leak during stats read
Procstat mistakenly assumed that 'forget' will be called immedeately after file
read (instead of close). This was an incorrect assumtion as kernel only calls
'forget' when inode is out of fscache. This lead to refcount leak and resulted in a
fact that stats that were deleted still present in tree.
This can lead to 2 issues:
   1) Prevent from recreating stats with same name
   2) On each "read" from stats refcount will be increased which will result in
refcounter wraparound and release.

src/procstat.c
@@ -207,18 +207,6 @@ static void free_directory(struct procstat_directory *directory)
 	free_item(&directory->base);
 }
 
-
-static void fuse_forget(fuse_req_t req, fuse_ino_t ino, unsigned long nlookup)
-{
-	struct procstat_item *item = fuse_inode_to_item(request_context(req), ino);
-
-	assert(item->refcnt >= nlookup);
-	item->refcnt -= nlookup;
-	if (item->refcnt == 0)
-		free_item(item);
-	fuse_reply_none(req);
-}
-
 #define INODE_BLK_SIZE 4096
 static void fill_item_stats(struct procstat_context *context, struct procstat_item *item, struct stat *stat)
 {
@@ -272,11 +260,10 @@ static void fuse_lookup(fuse_req_t req, fuse_ino_t parent_inode, const char *nam
 		fuse_reply_err(req, ENOENT);
 		return;
 	}
-	++item->refcnt;
-	pthread_mutex_unlock(&context->global_lock);
 
 	fuse_entry.ino = (uintptr_t)item;
 	fill_item_stats(context, item, &fuse_entry.attr);
+	pthread_mutex_unlock(&context->global_lock);
 	fuse_reply_entry(req, &fuse_entry);
 }
 
@@ -313,7 +300,7 @@ static void fuse_opendir(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *
 		fuse_reply_err(req, ENOENT);
 		return;
 	}
-
+	++item->refcnt;
 	pthread_mutex_unlock(&context->global_lock);
 	fuse_reply_open(req, fi);
 }
@@ -452,6 +439,8 @@ static void fuse_open(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
 
 	/* we dont know size of file in advance so use directio*/
 	fi->direct_io = true;
+
+	++item->refcnt;
 	fuse_reply_open(req, fi);
 }
 
@@ -1082,17 +1071,20 @@ void fuse_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr, int to_set,
 	fuse_reply_attr(req, &stat, 1.0);
 }
 
-void fuse_release(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
+static void fuse_release(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
 {
-	void *object = fi->fh;
+	struct procstat_context *context = request_context(req);
+	struct procstat_item *item = fuse_inode_to_item(request_context(req), ino);
 
-	free(object);
+	pthread_mutex_lock(&context->global_lock);
+	if (--item->refcnt == 0)
+		free_item(item);
+	pthread_mutex_unlock(&context->global_lock);
 	fuse_reply_err(req, 0);
 }
 
 static struct fuse_lowlevel_ops fops = {
 	.read = fuse_read,
-	.forget = fuse_forget,
 	.lookup = fuse_lookup,
 	.getattr = fuse_getattr,
 	.opendir = fuse_opendir,
@@ -1100,7 +1092,8 @@ static struct fuse_lowlevel_ops fops = {
 	.open = fuse_open,
 	.write = fuse_write,
 	.setattr = fuse_setattr,
-	.release = fuse_release
+	.release = fuse_release,
+	.releasedir = fuse_release,
 };
 
 #define ROOT_DIR_NAME "."

commit 532d0d120b0fc4712bdde107bc4821f86347bb38

procstat: fix refcount for control files
Refcount was mistakemly not increased for control files, this lead
to control file release during file access.

Fixes: e59946b ("procstat: fix refcount leak during stats read")

src/procstat.c
@@ -429,6 +429,7 @@ static void fuse_open(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
 
 	if (item_type_control(item)) {
 		fi->fh = 0;
+		++item->refcnt;
 		fuse_reply_open(req, fi);
 		return;
 	}