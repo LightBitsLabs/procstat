11f5894079631507c0fbd5ab9fa520fbde967809
Adir Gabai <adir@lightbitslabs.com>
2019-07-09 14:18:19 +0300

    procstat: align argument to function bla()
    According to the styling guide in lightbits, every argument should be in
    a separate line. bla bla blaf
    
    Issue: LBM1-XXXX

--- src/procstat.h
+++ src/procstat.h
@@ -337,8 +337,10 @@ int procstat_create_multiple_u64_series(struct procstat_context *context,
  */
 void procstat_u64_series_add_point(struct procstat_series_u64 *series, uint64_t value);
 
-int procstat_create_histogram_u32_series(struct procstat_context *context, struct procstat_item *parent,
-					 const char *name, struct procstat_histogram_u32 *series);
+int procstat_create_histogram_u32_series(struct procstat_context       *context,
+                                         struct procstat_item          *parent,
+                                         const char                    *name,
+                                         struct procstat_histogram_u32 *series);
 
 void procstat_histogram_u32_add_point(struct procstat_histogram_u32 *series, uint32_t value);
 
